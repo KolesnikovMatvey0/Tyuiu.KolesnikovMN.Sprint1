a8d2e7ba97a1b4cc66b2cd3e413d0f18242e6812
Kolesnikov Matvey <matveydev0@gmail.com>
2024-10-05 12:41:57 +0500

    Task1

--- Tyuiu.KolesnikovMN.Sprint1.Task1.V16/Program.cs
+++ Tyuiu.KolesnikovMN.Sprint1.Task1.V16/Program.cs
@@ -26,20 +26,21 @@ namespace Tyuiu.KolesnikovMN.Sprint1.Task1.V16
 
             double a, x, y;
 
-            Console.WriteLine("Введите значение Y:");
-            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
 
             Console.WriteLine("Введите значение X:");
             x = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите значение A:");
-            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(ds.Calculate(y, x, a));
+            Console.WriteLine(ds.Calculate(a, x, y));
             Console.ReadLine();
         }
     }
