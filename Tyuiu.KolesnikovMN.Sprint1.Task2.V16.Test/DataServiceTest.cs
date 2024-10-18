48d93d6043f4005817e39cb8db3adba719ca42c3
Kolesnikov Matvey <matveydev0@gmail.com>
2024-10-18 20:14:55 +0500

    Task2

--- Tyuiu.KolesnikovMN.Sprint1.Task2.V16.Test/DataServiceTest.cs
+++ Tyuiu.KolesnikovMN.Sprint1.Task2.V16.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.KolesnikovMN.Sprint1.Task2.V16.Test
         {
             DataService ds = new DataService();
             int x = 6;
-            var res = ds.CalculatePerimetrCircle(x) + 0.001;
+            var res = ds.CalculatePerimetrCircle(x) + 0.002;
             Assert.AreEqual(37.699, res);
         }
     }
